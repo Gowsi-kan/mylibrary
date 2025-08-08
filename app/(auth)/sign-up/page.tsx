0761f40d3ba4105a288f75b9d5cbe12725565a30
GowsiKan <gowsikan32412@gmail.com>
2025-08-09 00:49:20 +0530

    Hero, And Auth UI Completed

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import AuthForm from "@/components/AuthForm";
+import React from "react";
+import { signUpSchema } from "@/lib/validations";
+
+const page = () => (
+  <AuthForm
+    type="SIGN_UP"
+    schema={signUpSchema}
+    defaultValues={{
+      email: "",
+      password: "",
+      fullName: "",
+      universityId: 0,
+      universityCard: "",
+    }}
+  />
+);
+
+export default page;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-const Home = () => (
-  <>
-    <Button>Hello</Button>
-  </>
-);
-
-export default Home;
