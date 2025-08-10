74245fe1a33919b0441421f6c713c1140d11715f
GowsiKan <gowsikan32412@gmail.com>
2025-08-10 13:12:37 +0530

    image upload path changed

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -12,7 +12,7 @@ const {
 
 const authenticator = async () => {
   try {
-    const response = await fetch(`${config.env.apiEndPoint}/api/auth/imagekit`);
+    const response = await fetch(`${config.env.apiEndPoint}/api/imagekit`);
 
     if (!response.ok) {
       const errorText = await response.text();
