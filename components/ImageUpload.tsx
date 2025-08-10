82750d15a43ecdad5f58e1c91b744943bf30aae6
GowsiKan <gowsikan32412@gmail.com>
2025-08-10 11:28:49 +0530

    back to end point

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -12,9 +12,7 @@ const {
 
 const authenticator = async () => {
   try {
-    const response = await fetch(
-      `${config.env.prodApiEndPoint}/api/auth/imagekit`
-    );
+    const response = await fetch(`${config.env.apiEndPoint}/api/auth/imagekit`);
 
     if (!response.ok) {
       const errorText = await response.text();
